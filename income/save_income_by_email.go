f46e0665612f8d248427e3226333a685f86a4cfe
Vorrawut Judasri <vorrawutjud@gmail.com>
2019-11-15 16:47:42 +0700

    Create API getIncomeByEmail

---
+++ income/save_income_by_email.go
@@ -0,0 +1,65 @@
+package income
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+var ct time.Time
+
+type foodSelectedIDReq struct {
+	ID []int `json:"id"`
+}
+
+func (h *Handler) saveIncomeByEmail(c echo.Context) error {
+	uid := c.Param("id")
+
+	food := &foodSelectedIDReq{}
+	if err := c.Bind(food); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{
+			"code":    "F-5003",
+			"message": "RequestBody invalid",
+		})
+	}
+
+	return h.insertManageFoodByUserIDTable(c, food, uid)
+}
+
+func (h *Handler) insertManageFoodByUserIDTable(c echo.Context, food *foodSelectedIDReq, uid interface{}) error {
+	ct = time.Now()
+	ct.Format(time.RFC3339)
+
+	stmtDeletePayment := `DELETE FROM m_food WHERE user_id = ?;`
+	_, err := h.DB.Exec(stmtDeletePayment, uid)
+	if err != nil {
+		h.Logger(c).Errorf("deleteFoodByUserID error: %v", err)
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"code":    "F-5004",
+			"message": err,
+		})
+	}
+
+	for _, fid := range food.ID {
+
+		stmtIns := `INSERT INTO m_food (
+		food_id, user_id, created_date)
+		VALUES (?, ?, ?)`
+
+		_, err := h.DB.Exec(stmtIns,
+			fid,
+			uid,
+			ct)
+
+		if err != nil {
+			h.Logger(c).Errorf("insertManageFoodByUserIdTable error: %v", err)
+			return c.JSON(http.StatusInternalServerError, echo.Map{
+				"code":    "F-5004",
+				"message": "System error, please try again",
+			})
+		}
+	}
+
+	return c.JSON(http.StatusNoContent, nil)
+}
